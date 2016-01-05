12a17b0a8919e89b78ab1b9c5ea98886aa3020c0
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-01-05 22:43:27 +1300

    updated the db structure

--- db/structure.sql
+++ db/structure.sql
@@ -29,9 +29,24 @@ CREATE TABLE `accounts` (
   `display_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
+  `followers` int(11) NOT NULL,
+  `following` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Temporary view structure for view `all_nodes`
+--
+
+DROP TABLE IF EXISTS `all_nodes`;
+/*!50001 DROP VIEW IF EXISTS `all_nodes`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `all_nodes` AS SELECT 
+ 1 AS `username`,
+ 1 AS `display_name`*/;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `followers`
@@ -51,7 +66,7 @@ CREATE TABLE `followers` (
   PRIMARY KEY (`id`),
   KEY `fk_followers_accounts` (`account_id`),
   CONSTRAINT `followers_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3477 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +87,7 @@ CREATE TABLE `followings` (
   PRIMARY KEY (`id`),
   KEY `fk_followings_accounts` (`account_id`),
   CONSTRAINT `followings_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3065 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,6 +102,24 @@ CREATE TABLE `schema_migrations` (
   UNIQUE KEY `unique_schema_migrations` (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Final view structure for view `all_nodes`
+--
+
+/*!50001 DROP VIEW IF EXISTS `all_nodes`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `all_nodes` AS select `accounts`.`username` AS `username`,`accounts`.`display_name` AS `display_name` from `accounts` union select `followers`.`username` AS `username`,`followers`.`display_name` AS `display_name` from `followers` union select `followings`.`username` AS `username`,`followings`.`display_name` AS `display_name` from `followings` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -97,7 +130,7 @@ CREATE TABLE `schema_migrations` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-02 12:45:15
+-- Dump completed on 2016-01-05 22:42:53
 INSERT INTO schema_migrations (version) VALUES ('20151226225811');
 
 INSERT INTO schema_migrations (version) VALUES ('20151226234715');
